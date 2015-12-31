e73fa6881a6c04f112acf44eb29b7c33e968e5ff
priyankamandikal <priyanka.mp2808@gmail.com>
2015-12-31 13:19:46 +0530

    Added the first set of files to get the app running on the server
    Added the following files
    I have not added the database initializations as yet.This will be done in a later commit.
    This commit is just for setting up the basic app structure and displaying a welcome message on the screen when launched.
    
    /
    config.py - contains all the app configurations alongwit development, testing and production configuration settings
    manage.py - lauch script
    
    app/
    __init__.py - app initializations and imports necessary blueprints
    
    app/main/
    __init__.py - main blueprint
    errors.py - view for rendering error pages, calls respective html pages
    views.py - for calling index.html
    
    app/static/
    wikireview.ico - icon for the site
    
    app/templates/
    404.html - page not found error page
    500.html - error page for unhandled exceptions
    base.html - base html extending from bootstrap/base.html
    index.html - Home page, displays welcome message

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    WIKIREVIEW_MAIL_SUBJECT_PREFIX = '[Wikireview]'
+    WIKIREVIEW_MAIL_SENDER = 'gabriel.og.ucb@gmail.com'
+    WIKIREVIEW_ADMIN = os.environ.get('WIKIREVIEW_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
