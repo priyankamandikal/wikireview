ab7d86ec12e2d1d9301e6d8540e0fd3f92238f74
priyankamandikal <priyanka.mp2808@gmail.com>
2015-12-31 22:42:45 +0530

    New User Registration
    app/auth/forms.py - added the registration form that takes in email, username, password1 and password2 (for matching). It also has methods for validating email and username.
    app/templates/auth/register.html - registration template. Renders the registration form.
    app/templates/auth/login.html - added a link to registration page for new users
    app/auth/views.py - added the view for registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.login import login_user, logout_user, login_required, current_user
 from . import auth
+from .. import db
 from ..models import Reviewer
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -23,3 +24,15 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        reviewer = Reviewer(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(reviewer)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
